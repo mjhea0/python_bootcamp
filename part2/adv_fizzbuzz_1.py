4ae80742459d0da8737297e4f8ac0119f80da896
Michael Herman <hermanmu@gmail.com>
2014-09-18 07:35:24 -0500

    Update adv_fizzbuzz_1.py
    1. call the function in the main routine. it's the exact reason why it's there.
    2. return the output list, don't print inside the function. try to get away from printing inside a function.

--- part2/adv_fizzbuzz_1.py
+++ part2/adv_fizzbuzz_1.py
@@ -1,21 +1,40 @@
-# refactor to generate output to a list
 def fizzbuzz(upper_limit):
     output_list = []
-    for num in range(1, upper_limit+1):
-
-        if (num % 3 == 0 and num % 5 == 0):
+    for num in xrange(1, upper_limit+1):
+        if num % 15 == 0:
             output_list.append("FizzBuzz")
-
-        elif num % 5 == 0:
-            output_list.append("Buzz")
-
         elif num % 3 == 0:
             output_list.append("Fizz")
+        elif num % 5 == 0:
+            output_list.append("Buzz")
         else:
             output_list.append(num)
-    print(output_list)        
-
-
-print(fizzbuzz(35))
+    return output_list
 
 if __name__ == '__main__':
+    output = fizzbuzz(35)
+    for num in output:
+        print(num)
+
+
+# # refactor to generate output to a list
+# def fizzbuzz(upper_limit):
+#     output_list = []
+#     for num in range(1, upper_limit+1):
+
+#         if (num % 3 == 0 and num % 5 == 0):
+#             output_list.append("FizzBuzz")
+
+#         elif num % 5 == 0:
+#             output_list.append("Buzz")
+
+#         elif num % 3 == 0:
+#             output_list.append("Fizz")
+#         else:
+#             output_list.append(num)
+#     print(output_list)        
+
+
+# print(fizzbuzz(35))
+
+# if __name__ == '__main__':
